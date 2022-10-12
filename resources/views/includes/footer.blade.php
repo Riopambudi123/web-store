1354343f0d744e04f0591e7344311bd9e7f7a7d0
Riopambudi123 <riopambudi879@gmail.com>
2022-10-12 22:57:28 +0700

    webstore completed commit

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <div class="p pt-4 pb-2">
+                    2022 Copyright Store. All Rights Reserved | Developer By Rio Pambudi.
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
